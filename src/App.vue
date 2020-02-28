8958572fb88900e7b359ec1f47aefffcaeabae4d
sanyuan <sanyuan.jw@foxmail.com>
2020-02-28 22:44:14 +0800

    add g-button

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div id="app">
-    Hello Ginger
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <hr />
+    <g-button type="primary" @click="onClick" id="btn"> Click </g-button>
+    <g-button type="dashed" @click="onClick" id="btn"> Click </g-button>
+    <g-button @click="onClick" id="btn"> Click </g-button>
   </div>
 </template>
 
 <script>
 export default {
-  name: "app"
+  name: "app",
+  methods: {
+    onClick() {
+      console.log("click被触发");
+    }
+  }
 };
 </script>
 
