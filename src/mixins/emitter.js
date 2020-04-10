f97366c6164fbb3508e0ab0341ec726bd0fc41d1
Ginger <346600397@qq.com>
2020-04-10 21:28:01 +0800

    添加 input组件

---
+++ src/mixins/emitter.js
@@ -0,0 +1,40 @@
+/**
+ * 判断触发事件的组件
+ */
+function broadcast (componentName, eventName, params) {
+  console.log(componentName, eventName, params);
+  this.$children.forEach(child => {
+    const name = child.$options.name
+
+    if (name && name === componentName) {
+      child.$emit.apply(child, eventName.concat(params))
+    } else {
+      child.$emit.apply(child, [componentName, eventName].concat([params]))
+    }
+  })
+}
+
+export default {
+  methods: {
+    dispatch (componentName, eventName, params) {
+      console.log(componentName, eventName, params);
+      let parent = this.$parent || this.$root
+      let name = parent.$options.name
+
+      while (parent && (!name || name !== componentName)) {
+        parent = parent.$parent
+
+        if (parent) {
+          name = parent.$options.name
+        }
+      }
+
+      if (parent) {
+        parent.$emit.apply(parent, [eventName].concat(params))
+      }
+    },
+    broadcast (componentName, eventName, params) {
+      broadcast.call(this, componentName, eventName, params)
+    }
+  }
+}
