eb55780f3261d65ea3cfd76078ba725e7c7054cc
sanyuan <sanyuan.jw@foxmail.com>
2020-04-10 21:28:01 +0800

    添加 input组件

---
+++ src/components/Input/input.vue
@@ -0,0 +1,15 @@
+<template>
+  <input
+    type="text"
+    :value="currentValue"
+    @input="handleInput"
+    @blur="handleBlur"
+  />
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
