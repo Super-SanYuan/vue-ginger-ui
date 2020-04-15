edeefddf647322c18da1bf262f66ee9f98cf6d24
sanyuan <sanyuan.jw@foxmail.com>
2020-04-15 20:49:13 +0800

    add form and form-item

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,47 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <hr />
-    <g-button type="primary" @click="onClick" id="btn"> Click </g-button>
-    <g-button type="dashed" @click="onClick" id="btn"> Click </g-button>
-    <g-button @click="onClick" id="btn"> Click </g-button>
+    <g-form :model="formValidate" :rules="rulesValidate">
+      <g-form-item label="用户名" prop="name">
+        <!-- <g-input v-model="formValidate.name"></g-input> -->
+        <input type="text" v-model="formValidate.name" />
+      </g-form-item>
+      <g-form-item label="邮箱" prop="email">
+        <!-- <g-input v-model="formValidate.email"></g-input> -->
+        <input type="text" v-model="formValidate.email" />
+      </g-form-item>
+    </g-form>
+    <button @click="handleClick">CLick</button>
   </div>
 </template>
 
 <script>
+import Form from "./components/Form/form";
+import formItem from "./components/Form/form-item";
 export default {
   name: "app",
+  components: { "g-form": Form, "g-form-item": formItem },
+  data() {
+    return {
+      formValidate: {
+        name: "",
+        email: ""
+      },
+      rulesValidate: {
+        name: [{ requied: true, message: "用户名不能为空", trigger: "blur" }],
+        email: [
+          { required: true, message: "邮箱不能为空", trigger: "blur" },
+          { type: "email", message: "邮箱格式不正确", trigger: "blur" }
+        ]
+      }
+    };
+  },
+  mounted() {
+    console.log(this.$options.name);
+  },
   methods: {
-    onClick() {
-      console.log("click被触发");
+    handleClick() {
+      this.$emit("click", 1);
     }
   }
 };
---
+++ src/components/Form/form-item.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <label v-if="label">{{ label }}</label>
+    <div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import Emitter from "../../mixins/emitter";
+export default {
+  name: "gFormItem",
+  mixins: [Emitter],
+  props: {
+    label: {
+      type: String,
+      default: ""
+    },
+    prop: {
+      type: String
+    }
+  },
+  mounted() {
+    if (this.porp) {
+      this.dispatch("gForm", "on-form-item-add", this);
+    }
+  },
+  beforeDestroy() {
+    this.dispatch("gForm", "on-form-item-remove", this);
+  }
+};
+</script>
+
+<style>
+</style>
