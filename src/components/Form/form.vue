174a064f8d35b8ba141b0b8fcb6dc3740a47c149
Ginger <346600397@qq.com>
2020-04-15 20:49:13 +0800

    add form and form-item

---
+++ src/components/Form/form.vue
@@ -0,0 +1,30 @@
+<template>
+  <form>
+    <slot></slot>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "gForm",
+  props: {
+    model: {
+      type: Object
+    },
+    rules: {
+      type: Object
+    }
+  },
+  created() {
+    this.$on("on-form-item-add", field => {
+      if (field) this.field.push(field);
+    });
+    this.$on("on-form-item-remove", field => {
+      if (field.prop) this.field.splice(this.field.indexOf(field), 1);
+    });
+  }
+};
+</script>
+
+<style lang="less">
+</style>
